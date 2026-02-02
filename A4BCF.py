b0ca793f56e7b4c60f49d509cc16744afc9d2dc8
Andrew Hunt <ach221@maxi>
2026-02-02 19:57:34 +0000

    trying to do the residue integrals, looks like theres a bug atm

--- A4BCF.py
+++ A4BCF.py
@@ -29,8 +29,16 @@ class A4_BCF:
         Set the pole-residue representation of J(w).
         J(w) = sum_k residues[k] / (w - poles[k])
         """
-        self.Jw_pol = np.asarray(poles)
-        self.Jw_res = np.asarray(residues)
+        poles=np.asarray(poles)
+        residues=np.asarray(residues)
+
+        self.Jw_pol_pos = poles[np.imag(poles)>0]
+        self.Jw_pol_neg = poles[np.imag(poles)<0]
+        self.Jw_pol = np.concatenate([self.Jw_pol_pos,self.Jw_pol_neg])
+
+        self.Jw_res_pos = residues[np.imag(poles)>0]
+        self.Jw_res_neg = residues[np.imag(poles)<0]
+        self.Jw_res =  np.concatenate([self.Jw_res_pos,self.Jw_res_neg])
 
         if len(self.Jw_pol) != len(self.Jw_res):
             raise ValueError("poles and residues must have same length")
@@ -42,7 +50,7 @@ class A4_BCF:
         if self.Jw_pol is None: raise RuntimeError("Spectral density not set")
         return np.sum(self.Jw_res / (w - self.Jw_pol), axis=0)
 
-    def plot_J(self, wmin, wmax, npts=1000):
+    def plot_J(self, wmin, wmax, npts=1000, show=False):
         """
         Plot the spectral density.
         """
@@ -54,7 +62,7 @@ class A4_BCF:
         plt.ylabel(r'$J(\omega)$')
         plt.legend()
         plt.tight_layout()
-        plt.show()
+        if show: plt.show()
 
     # --------------------------------------------------
     # BCF
@@ -62,25 +70,69 @@ class A4_BCF:
     def compute_bcf(self, doplot=False):
         """
         Compute the bath correlation function using A4 decomposition.
+
+        Outputs in form 
+
+        C(t) = \sum_n kap_n \exp{gam_n t} + delta(t)
         """
         if self.Jw_pol is None: raise RuntimeError("Spectral density not set")
 
-
+        # Get the Radius of Gyration in k, eta form then convert to poles and res and const
         self.eta, self.k = self.A4decomp.compute(doplot=doplot)
+        self.Rg_pol_pos = 1.j*self.eta[1:]
+        self.Rg_pol_neg = -1.j*self.eta[1:]
+        self.Rg_pol = np.concatenate([self.Rg_pol_pos,self.Rg_pol_neg])
 
-        self.Rg_pol = 1.j*np.concatenate([self.eta[1:],-self.eta[1:]])
-        self.Rg_res = np.concatenate([self.k[1:],self.k[1:]])/(self.Rg_pol*2.)
+        self.Rg_res_pos = self.k[1:]/(self.Rg_pol_pos*2.)
+        self.Rg_res_neg = self.k[1:]/(self.Rg_pol_neg*2.)
+        self.Rg_res = np.concatenate([self.Rg_res_pos,self.Rg_res_neg])
+        self.Rg_con = self.k[0]
 
-        if(1):
+        if(0): #check we havent messed up out conversions
             w=np.linspace(-15,15,1000)
             plt.plot(w,np.sum(self.Rg_res[:,None]/(w[None,:]-self.Rg_pol[:,None]),axis=0))
             plt.plot(w,np.imag(np.sum(self.Rg_res[:,None]/(w[None,:]-self.Rg_pol[:,None]),axis=0)),label='imag')
             plt.plot(w,np.sum(self.k[1:,None]/(self.eta[1:,None]**2+w[None,:]**2),axis=0),label='with etas',linestyle='--',color='k')
-            
             plt.legend()
             plt.show()
 
-        # TO BE CONTINUED
+        # Find the frequencies and the Masks for the Jw and Rg coeffs
+        self.pol_pos=np.concatenate([self.Jw_pol_pos,self.Rg_pol_pos])
+        self.gam=1j*self.pol_pos
+
+        n_Jw=len(self.Jw_pol_pos)
+        Jw_idx = np.arange(n_Jw)
+
+
+        # Find the prefactors [more difficult...]
+        self.kap=np.zeros_like(self.gam,dtype=complex)
+
+        # 1) Add on the classical stuff IGNORING 1/w PV (as it must cancel by symmetry)
+        self.kap[Jw_idx] += (2.j/self.beta)*self.Jw_res_pos/self.Jw_pol_pos
+        # 2) Add on the imaginary stuff
+        self.kap[Jw_idx] -=  1.j*self.hbar*self.Jw_res_pos
+        # 3) Add on the Rg poles
+        def res_helper(residues,poles,target_pole):
+            ''' Calculates the function, but skips out the 1/0 divergence if present'''
+            result=0+0.j
+            for residue_i,pole_i in zip(residues,poles):
+                if not np.isclose(pole_i, target_pole):
+                    result+=residue_i/(target_pole-pole_i)
+            return result
+
+        for indx,pol_pos_i in enumerate(self.pol_pos):
+            self.kap[indx] += 2.j * res_helper(self.Jw_res,self.Jw_pol,pol_pos_i) * res_helper(self.Rg_res,self.Rg_pol,pol_pos_i)*pol_pos_i
+
+        return self.kap,self.gam
+        
+
+
+
+                
+
+
+
+        
 
         # self.bcf_modes = list(zip(list_g, list_w))
         # return self.bcf_modes
@@ -88,14 +140,24 @@ class A4_BCF:
 if __name__ == '__main__':
     bcf = A4_BCF(beta=11, hbar=1.2, K=3, distribution='Bose')
 
-    poles = [1j, -1j]
-    residues = [0.5, 0.5]
 
-    poles = [1j, -1j]
-    residues = [0.5, 0.5]
+    # lets try one - for the example of Debye bath
+    eta_DL=2
+    gam_DL=2
 
-    bcf.set_spectral_density(poles, residues)
-    # bcf.plot_J(-5, 5)
+    Jw_residues = [eta_DL*gam_DL/2, eta_DL*gam_DL/2]
+    Jw_poles=[1.j*gam_DL,-1.j*gam_DL]
 
-    modes = bcf.compute_bcf(doplot=0)
-    print(modes)
+
+
+    bcf.set_spectral_density(Jw_poles, Jw_residues)
+    if(0):
+        bcf.plot_J(-5, 5)
+        w = np.linspace(-5, 5, 1000)
+        plt.plot(w,eta_DL*gam_DL*w/(w**2+gam_DL**2),linestyle='--',color='k')
+        plt.show()
+
+
+    kap,gam = bcf.compute_bcf(doplot=0)
+    print('kap',kap)
+    print('gam',gam)
